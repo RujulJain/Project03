a5a817b2d63e33c2ca83d501bd645702905b0865
Rujul Jain <rsj4000@gmail.com>
2020-11-23 17:08:11 -0600

    Added Visual/Audio Feedback and Speed panel

---
+++ Assets/Scripts/DisplaySpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplaySpeed : MonoBehaviour
+{
+    public Text _currentSpeed;
+    public CharacterController cc;
+
+    private void Start()
+    {
+        _currentSpeed.text = "Speed: 0";
+    }
+
+    private void Update()
+    {
+        _currentSpeed.text = "Speed: " + cc.velocity.magnitude.ToString("F1");
+    }
+}
---
+++ Assets/Scripts/WalkScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkScript : MonoBehaviour
+{
+    // Use this for initialization
+    CharacterController cc;
+    new AudioSource audio;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && audio.isPlaying == false)
+        {
+            audio.volume = Random.Range(0.8f, 1);
+            audio.pitch = Random.Range(0.8f, 1);
+            audio.Play();
+        }
+    }
+}
